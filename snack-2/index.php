1da2aa199a9a9c0c140513a69fa4bf712cd33306
Onda-dev <manuelaltieri3@gmail.com>
2022-06-22 16:00:50 +0200

    Snack 2 ok

--- snack-2/index.php
+++ snack-2/index.php
@@ -1,10 +1,28 @@
 <!-- Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
 
 <?php
+
 $name = $_GET['name'];
 $mail = $_GET['mail'];
 $age = $_GET['age'];
 
+if (strlen($name) > 3){
+    echo "Nome inoltrato correttamente";
+} else {
+    echo "$nome è troppo breve";
+}
+
+if (!filter_var($mail, FILTER_VALIDATE_EMAIL)){
+    echo "L'indirizzo email $mail è valido";
+} else {
+    echo "L'indirizzo email $mail non è valido";
+}
+
+if (is_numeric($age)){
+    echo "Età inoltrata correttamente";
+} else {
+    echo "$age non è un numero";
+}
 ?>
 
 <!DOCTYPE html>
