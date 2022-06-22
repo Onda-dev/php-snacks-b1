dd3edab80cb97f22cc29b271910ec9f0b5206ce2
Onda-dev <manuelaltieri3@gmail.com>
2022-06-22 15:00:08 +0200

    Array creato

--- snack-1/index.php
+++ snack-1/index.php
@@ -1,2 +1,50 @@
 <!-- Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
 Olimpia Milano - Cantù | 55-60 -->
+<?php
+$partite = [
+    [
+        'squadra_casa' => 'Olimpia Milano',
+        'squadra_ospite' => 'Cantù',
+        'punti_casa' => 55,
+        'punti_ospite' => 60
+    ],
+    [
+        'squadra_casa' => 'Tortoreto',
+        'squadra_ospite' => 'Atletico Adriatico',
+        'punti_casa' => 50,
+        'punti_ospite' => 70
+    ],
+    [
+        'squadra_casa' => 'Teramo',
+        'squadra_ospite' => 'Salernitana',
+        'punti_casa' => 70,
+        'punti_ospite' => 80
+    ],
+    [
+        'squadra_casa' => 'Verdy',
+        'squadra_ospite' => 'Tokyo',
+        'punti_casa' => 55,
+        'punti_ospite' => 65
+    ],
+]
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="partita">
+        <div class="squadre">
+            <?php ?>
+        </div>
+        <div class="risultato">
+
+        </div>
+    </div>
+</body>
+</html>
